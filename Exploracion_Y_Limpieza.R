ab9beaee3c9ec4b22f00eb1266f2533e26b35771
Aritz <aritz.saavedra@alumni.mondragon.edu>
2025-06-04 09:10:10 +0200

    "exploracion hecha"

--- Exploracion_Y_Limpieza.R
+++ Exploracion_Y_Limpieza.R
@@ -1,1 +1,25 @@
-  
+library(digest)
+library(discretization)
+library(dplyr)
+library(EnvStats)
+library(fastDummies)
+library(naniar)
+library(nortest)
+library(readxl)
+library(smoothmest)
+library(VIM)
+library(plumber)
+
+
+df<-read.csv("netflix.csv")
+
+summary(df)
+
+sd(df$Minutes,na.rm = T)
+sd(df$Seasons,na.rm = T)
+
+unique(df$type)
+unique(df$country)
+unique(df$director)
+
+x<-df %>% group_by(df$director) %>% summarise(n())
